dc713e12dfe7005176711930e138c84f0fe03b67
Jonas Jore <jonas.jore@gmail.com>
2018-03-21 18:55:57 +0100

    sum of numbers in a consecutive row of odd numbers

---
+++ challenges/sumOfOddNumbers.js
@@ -0,0 +1,15 @@
+/*
+Given this triangle of consecutive odd numbers:
+calculate the row sums of this triangle from the row index
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+
+*/
+
+function rowSumOddNumbers(n) {
+	return Math.pow(n,3);
+}
