5eba70d3a5e88882bdf38a64cd924e4889e32980
Jonas Jore <jonas.jore@gmail.com>
2018-09-16 23:34:40 +0200

    reverse!

---
+++ a-challenge-a-day/ReverseStringKeepSpacing.java
@@ -0,0 +1,19 @@
+import java.lang.StringBuilder;
+
+public class ReverseStringKeepSpacing {
+  public static String reverseWords(final String original) {
+    String[] words = original.split(" ");
+    StringBuilder builder = new StringBuilder();
+    for(int i = 0; i < words.length; i++) {
+      StringBuilder sb = new StringBuilder(words[i]);
+      words[i] = sb.reverse().toString();
+      if(i == words.length-1) {
+        builder.append(words[i]);
+      } else {
+        builder.append(words[i] + " ");
+      }
+    }
+
+    return builder.toString();
+  }
+}
