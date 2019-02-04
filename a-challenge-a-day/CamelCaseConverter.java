c7d4a6549a2662f99fee8b2f6d17897e45173039
Jonas <jonas.jore@gmail.com>
2019-02-04 22:58:04 +0100

    turned method into a oneliner

--- a-challenge-a-day/CamelCaseConverter.java
+++ a-challenge-a-day/CamelCaseConverter.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class CamelCaseConverter {
@@ -7,22 +6,16 @@ public class CamelCaseConverter {
   public static void main(String[] args) {
     String test = new CamelCaseConverter().camelCase("camel case word");
     System.out.println(test);
-    String test2 = new CamelCaseConverter().camelCase("hello world");
+    String test2 = new CamelCaseConverter().camelCase(" hello world ");
     System.out.println(test2);
     String test3 = new CamelCaseConverter().camelCase("Camelize this");
     System.out.println(test3);
   }
 
   public static String camelCase(String str) {
-    List<String> split = Arrays.asList(str.split(" "));
-    List<String> collect = split.stream()
-        .filter(s -> !s.isEmpty() || s != null)
-        .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
-        .collect(Collectors.toList());
-
-    String resultStr = String.join("", collect);
-
-    return resultStr;
+    return String.join("", Arrays.stream(str.split(" "))
+        .filter(item -> !item.isEmpty())
+        .map(item -> item.substring(0, 1).toUpperCase() + item.substring(1))
+        .collect(Collectors.toList()));
   }
-  
 }
