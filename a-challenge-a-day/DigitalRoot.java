10455f5a7d701aa3964854f223fe7708d299f8e9
Jonas <jonas.jore@gmail.com>
2019-02-05 19:52:59 +0100

    find digital root of given number algo

---
+++ a-challenge-a-day/DigitalRoot.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DigitalRoot {
+  
+  public static void main(String[] args) {
+    int i = new DigitalRoot().digital_root(234567);
+    System.out.println(i);
+  }
+
+  public static int digital_root(int n) {
+    List<String> digits = Arrays.asList(String.valueOf(n).split(""));
+    List<Integer> digs = digits.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+
+    if(digs.size() == 1) {
+      return n;
+    }
+
+    int sum = 0;
+
+    for(int i = 0; i < digs.size(); i++) {
+      sum += digs.get(i);
+    }
+
+    if(String.valueOf(sum).length() != 1) {
+      return digital_root(sum);
+    }
+
+    return sum;
+  }
+
+}
