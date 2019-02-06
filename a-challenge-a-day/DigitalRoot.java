87fdf52d3cc30f6686eb3ae638a49adfd7a09c14
Jonas <jonas.jore@gmail.com>
2019-02-06 15:45:31 +0100

    refactor :coffee:

--- a-challenge-a-day/DigitalRoot.java
+++ a-challenge-a-day/DigitalRoot.java
@@ -3,16 +3,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DigitalRoot {
-  
+
   public static void main(String[] args) {
-    int i = new DigitalRoot().digital_root(234567);
-    System.out.println(i);
+    System.out.println(new DigitalRoot().digital_root(234567));
   }
 
   public static int digital_root(int n) {
     List<String> digits = Arrays.asList(String.valueOf(n).split(""));
     List<Integer> digs = digits.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
-
     if(digs.size() == 1) {
       return n;
     }
@@ -26,8 +24,6 @@ public class DigitalRoot {
     if(String.valueOf(sum).length() != 1) {
       return digital_root(sum);
     }
-
     return sum;
   }
-
 }
