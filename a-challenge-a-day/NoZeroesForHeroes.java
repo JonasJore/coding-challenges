146d6c4fa21fd707d677631afe21752a167e2ee4
JonasJore <jonas.jore@gmail.com>
2019-02-19 19:55:27 +0100

    rewrote the logic. saved some lines!

--- a-challenge-a-day/NoZeroesForHeroes.java
+++ a-challenge-a-day/NoZeroesForHeroes.java
@@ -5,28 +5,38 @@
 public class NoZerosForHeroes {
 
   public static void main(String[] args) {
-    System.out.println(noBoringZeros(-1050));
+    System.out.println(noBoringZeros(960000));
+    System.out.println(noBoringZerosRewrite(960000) + "\t <- rewrite here");
   }
 
   public static int noBoringZeros(int n) {
     if(n == 0) {
       return 0;
     }
+    
     int trailingZeroes = 0;
     int nCopy = n;
     while(nCopy % 10 == 0) {
       ++trailingZeroes;
       nCopy /= 10;
     }
-
+    
     String stringNum = "1";
     for(int i = 0; i < trailingZeroes; i++) {
       stringNum += "0";
     }
-
+    
     int divisor = Integer.parseInt(stringNum);
-
     return n / divisor;
-    // return nCopy;
+  }
+
+  public static int noBoringZerosRewrite(int n) {
+    if(n == 0) {
+      return n;
+    }
+    while(n % 10 == 0) {
+      n /= 10;
+    }
+    return n;
   }
 }
