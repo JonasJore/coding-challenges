4c4732ec4ffcc620be9cac4af95ce1c5f26ceaec
jonas <jonas.jore@gmail.com>
2020-06-05 00:20:15 +0200

    welcome challenge

---
+++ CodeWars/python/welcome.py
@@ -0,0 +1,50 @@
+# https://www.codewars.com/kata/577ff15ad648a14b780000e7
+
+# first, quick approach:
+database = {
+'english': 'Welcome',
+'czech': 'Vitejte',
+'danish': 'Velkomst',
+'dutch': 'Welkom',
+'estonian': 'Tere tulemast',
+'finnish': 'Tervetuloa',
+'flemish': 'Welgekomen',
+'french': 'Bienvenue',
+'german': 'Willkommen',
+'irish': 'Failte',
+'italian': 'Benvenuto',
+'latvian': 'Gaidits',
+'lithuanian': 'Laukiamas',
+'polish': 'Witamy',
+'spanish': 'Bienvenido',
+'swedish': 'Valkommen',
+'welsh': 'Croeso'
+}
+
+def greet(language):
+  if language in database:
+    return database[language]
+
+  return database['english']
+
+# second approach after some thinking if i could oneline this function:
+def greet(language):
+    return {
+      'english': 'Welcome',
+      'czech': 'Vitejte',
+      'danish': 'Velkomst',
+      'dutch': 'Welkom',
+      'estonian': 'Tere tulemast',
+      'finnish': 'Tervetuloa',
+      'flemish': 'Welgekomen',
+      'french': 'Bienvenue',
+      'german': 'Willkommen',
+      'irish': 'Failte',
+      'italian': 'Benvenuto',
+      'latvian': 'Gaidits',
+      'lithuanian': 'Laukiamas',
+      'polish': 'Witamy',
+      'spanish': 'Bienvenido',
+      'swedish': 'Valkommen',
+      'welsh': 'Croeso'
+    }.get(language, "Welcome")
