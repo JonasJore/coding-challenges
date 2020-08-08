75a005a0e53aa2ad90642ceccc2e717c979a1e51
jonas <jonas.jore@gmail.com>
2020-08-08 14:29:09 +0200

    fix

--- CodeWars/rust/messi_goals.rs
+++ CodeWars/rust/messi_goals.rs
@@ -1,7 +1,10 @@
 // https://www.codewars.com/kata/55ca77fa094a2af31f00002a/rust
 
-static la_liga_goals: u32 = 43;
-static champions_league_goals: u32 = 10;
-static copa_del_rey_goals: u32 = 5;
+fn messi_goals() -> u32 {
+  static la_liga_goals: u32 = 43;
+  static champions_league_goals: u32 = 10;
+  static copa_del_rey_goals: u32 = 5;
 
-static total_goals: u32 = la_liga_goals + champions_league_goals + copa_del_rey_goals;
+  (la_liga_goals + champions_league_goals + copa_del_rey_goals)
+}
+
