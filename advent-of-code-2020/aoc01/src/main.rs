38e79a9b6f0341cff786dc1bd1270a7ce98c6680
JonasJore <jonas.jore@gmail.com>
2020-12-01 22:24:50 +0100

    solved aoc01 :santa:

--- advent-of-code-2020/aoc01/src/main.rs
+++ advent-of-code-2020/aoc01/src/main.rs
@@ -1,19 +1,49 @@
 use std::io::{ self, Read };
 
-type Resulted<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
+type Res<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
-fn main() -> Resulted<()> {
+fn parse_file(input: &str) -> Result<Vec<u32>, ::std::num::ParseIntError> {
+    input.lines()
+        .flat_map(|line| line.split_whitespace())
+        .map(|number| {
+            number.parse::<u32>()
+        }).collect()
+}
+
+fn main() -> Res<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    for line in input.lines() {
-        println!("{}", &line);
-    }
+    let vector: Vec<u32> = parse_file(&input).unwrap();
+
+    println!("answer: part 1: {}", part1(&vector));
+    println!("answer: part 2: {}", part2(&vector));
 
     Ok(())
 }
 
-fn part1(input: &str) -> Resulted<()> {Ok(())}
-fn part2(input: &str) -> Resulted<()> {Ok(())}
+fn part1(vec: &Vec<u32>) -> u32 {
+    for i in 0..vec.len() {
+        for j in i + 1..vec.len() {
+            if vec[i] + vec[j] == 2020 {
+                return vec[i] * vec[j];
+            }
+        }
+    }
+    0
+}
+
+fn part2(vec: &Vec<u32>) -> u32 {
+    for i in 0..vec.len() {
+        for j in i + 1..vec.len() {
+            for k in j+1..vec.len() {
+                if vec[i] + vec[j] + vec[k] == 2020 {
+                    return vec[i] * vec[j] * vec[k];
+                }
+            }
+        }
+    }
+    0
+}
 
 
