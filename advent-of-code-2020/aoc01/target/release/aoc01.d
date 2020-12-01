3e5e6e7cec4fff99e5f15e54a4ea1adc92c9c875
JonasJore <jonas.jore@gmail.com>
2020-12-01 01:19:49 +0100

    deleted unneeded files

--- advent-of-code-2020/aoc01/target/release/aoc01.d
+++
@@ -1,1 +0,0 @@
-C:\Users\jonas\code\coding-challenges\advent-of-code-2020\aoc01\target\release\aoc01.exe: C:\Users\jonas\code\coding-challenges\advent-of-code-2020\aoc01\src\main.rs
--- advent-of-code-2020/aoc01/target/release/deps/aoc01.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jonas\code\coding-challenges\advent-of-code-2020\aoc01\target\release\deps\aoc01.exe: src\main.rs
-
-C:\Users\jonas\code\coding-challenges\advent-of-code-2020\aoc01\target\release\deps\aoc01.d: src\main.rs
-
-src\main.rs:
