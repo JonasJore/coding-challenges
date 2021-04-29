6735f507c8ae7a298b672fb1bc7d24e7e36864bc
JonasJore <jonas.jore@gmail.com>
2021-04-29 23:10:09 +0200

    python challenge for solving area rounded to two decimals of a cirlce!

---
+++ CodeWars/python/circleArea.py
@@ -0,0 +1,9 @@
+#https://www.codewars.com/kata/537baa6f8f4b300b5900106c/python
+import math
+
+def circle_area(r):
+    if type(r) == str:
+        return False
+    if r < 1:
+        return False
+    return round(math.pi * (round(r, 2)**2), 2)
