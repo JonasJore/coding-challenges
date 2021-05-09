483069f9acec97fa3e4063f754cd0f791f2cfd9b
JonasJore <jonas.jore@gmail.com>
2021-05-09 03:56:53 +0200

    late night coding is not a good idea

--- daily-programmer/talking_clock.rs
+++ daily-programmer/talking_clock.rs
@@ -1,6 +1,11 @@
 // https://www.reddit.com/r/dailyprogrammer/comments/6jr76h/20170627_challenge_321_easy_talking_clock/
 use std::collections::HashMap;
 
+struct Clock<'a> {
+    hours: &'a str,
+    minutes: &'a str
+}
+
 fn main() {
     let hours_map: HashMap<&str, &str> = [
         ("01", "one"),
@@ -21,7 +26,6 @@ fn main() {
     .collect();
 
     let minutes_map: HashMap<&str, &str> = [
-        ("00", ""),
         ("01", "one"),
         ("02", "two"),
         ("03", "three"),
@@ -57,42 +61,21 @@ fn main() {
     .cloned()
     .collect();
 
-    let input = "09:00";
+    let input = "00:00";
 
-    let mut split_input: Vec<&str> = input.split(':').collect();
-    let hours_to_words = match split_input[0].parse::<i32>().unwrap() {
-        hour if hour < 10 => hours_map[&format!("0{}", hour).to_string().as_str()],
-        hour if hour <= 24 && hour > 9 => {
-            hours_map[&format!("0{}", (hour - 12)).to_string().as_str()]
-        }
-        _ => "not valid hour",
+    let input_split: Vec<&str> = input.split(':').collect();
+
+    let clock = Clock {hours: &input_split[0], minutes: &input_split[1] };
+
+    let hours_as_int = clock.hours.parse::<i32>().unwrap();
+
+    // hele klokkeslett er første case
+    // deretter klokkeslett som It's one thirty am
+    // også dette it's twelve thirty four am (kombinerer både tier og enkeltminutt)
+
+    let am_pm = if hours_as_int < 12 {
+        "am"
+    } else {
+        "pm"
     };
-
-    let minute_vect: Vec<char> = split_input[1].chars().collect();
-
-    let minute_ten_indicator = minute_vect[0].to_digit(10).unwrap();
-
-    let tens = match minute_ten_indicator {
-        ten if ten > 0 => minute_tens_map[ten.to_string().as_str()],
-        _ => "not valid",
-    };
-
-    let minute_to_text = minutes_map[split_input[1]];
-
-    println!("{}", minute_to_text);
-    let am_pm = match split_input[0].parse::<i32>().unwrap() {
-        h if h > 12 => "pm",
-        _ => "am",
-    };
-
-    let mut timestamp: String = String::new();
-
-    let minute_indicator = split_input[1].parse::<i32>().unwrap();
-
-    // happycase covered!
-    if minute_to_text == "" {
-        timestamp = format!("It's {} {}", hours_to_words, am_pm);
-    }
-
-    println!("{}", timestamp);
 }
