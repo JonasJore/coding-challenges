12717368930324601174df9363a609b4f9484aa6
pbv2 <pbv2@cin.ufpe.br>
2021-10-07 19:32:25 -0300

    add createPhoneNumber challenge from CodeWars in Java

---
+++ CodeWars/java/CreatePhoneNumber.java
@@ -0,0 +1,5 @@
+public class Kata {
+  public static String createPhoneNumber(int[] numbers) {
+    return("(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9]);
+  }
+}
