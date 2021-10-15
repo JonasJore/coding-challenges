10685cb158580aadcd4ccdc9b5311d61f8e4c64d
pbv2 <pbv2@cin.ufpe.br>
2021-10-15 18:52:00 -0300

    link to the codewars challenge and tests

--- CodeWars/java/CreatePhoneNumber.java
+++ CodeWars/java/CreatePhoneNumber.java
@@ -3,3 +3,11 @@ public class Kata {
     return("(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9]);
   }
 }
+
+public class PhoneExampleTests {
+  @Test
+  public void tests() {
+  assertEquals("(123) 456-7890", Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+  assertEquals("(987) 654-3210", Kata.createPhoneNumber(new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}));
+  }
+}
