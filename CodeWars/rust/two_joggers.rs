ae131eb806243add183d05812c506227e189e1a2
JonasJore <jonas.jore@gmail.com>
2022-07-03 23:20:08 +0200

    added editorconfig file for formatting. maybe different formatting for each type of files later

--- CodeWars/rust/two_joggers.rs
+++ CodeWars/rust/two_joggers.rs
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/5274d9d3ebc3030802000165
 fn nbr_of_laps (x: u16, y: u16) -> (u16, u16) {
   let mut laps_ran: u16 = x;
-  
+
   while laps_ran % y != 0 {
     laps_ran += x;
   }
