15905fe76425e0d5ab485d7d6cd0edd75539b2d1
pbv2 <pbv2@cin.ufpe.br>
2022-10-02 01:39:45 -0300

    leetCode - python - mergeTwoLists

---
+++ Leetcode/python/mergeTwoLists.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        head = flag = ListNode()
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                head.next = list1
+                list1 = list1.next
+            else:
+                head.next = list2
+                list2 = list2.next
+            head = head.next
+        
+        if list1:
+            head.next = list1
+        elif list2:
+            head.next = list2
+            
+        return flag.next
