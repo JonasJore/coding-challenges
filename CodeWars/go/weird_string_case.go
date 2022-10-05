589ab53d01027f3dbc82ffd7f418ffd136a55291
JonasJore <jonas.jore@gmail.com>
2022-10-05 09:18:50 +0200

    enhanced the weird string converter - tests at codewars are scuffed

--- CodeWars/go/weird_string_case.go
+++ CodeWars/go/weird_string_case.go
@@ -12,20 +12,19 @@ func isEven(index int) bool {
 }
 
 func toWeirdCase(str string) string {
-    chars := strings.Split(str, "")
+    words := strings.Split(str, " ")
     var weirdArr []string
-    for i := 0; i < len(chars); i++ {
-        if isEven(i) {
-            weirdArr = append(weirdArr, strings.ToUpper(chars[i]))
-        } else {
-            if strings.ToUpper(chars[i]) == chars[i] {
-                weirdArr = append(weirdArr, strings.ToLower(chars[i]))
+    for _, word := range words {
+        chars := strings.Split(word, "")
+        for index, char := range chars {
+            if isEven(index) {
+                weirdArr = append(weirdArr, strings.ToUpper(char))
             } else {
-                weirdArr = append(weirdArr, chars[i])
+                weirdArr = append(weirdArr, char)
             }
         }
+        weirdArr = append(weirdArr, " ")
     }
-
     return strings.Join(weirdArr, "")
 }
 
