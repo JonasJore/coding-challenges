f775ca2e0c3cf1e46d47bc5d0ffe760fcd0226a0
JonasJore <jonas.jore@gmail.com>
2022-12-01 10:24:09 +0100

    AoC day 01 �

--- advent-of-code-2022/src/main/kotlin/Day01.kt
+++ advent-of-code-2022/src/main/kotlin/Day01.kt
@@ -4,8 +4,33 @@ internal class Day01 {
     private var input: List<String> = readInputToArr("day01_input.txt")
 
     fun part1() {
-        println(input)
+        var maxCalories = 0
+        val food = mutableListOf<Int>()
+        for (line in input) {
+            if (line == "") {
+                if (food.sum() > maxCalories) {
+                    maxCalories = food.sum()
+                }
+                food.clear()
+            } else {
+                food.add(line.toInt())
+            }
+        }
+        println(maxCalories)
     }
 
-    fun part2() {}
+    fun part2() {
+        val maxCalories = mutableListOf<Int>()
+        val food = mutableListOf<Int>()
+
+        for (line in input) {
+            if (line == "") {
+                maxCalories.add(food.sum())
+                food.clear()
+            } else {
+                food.add(line.toInt())
+            }
+        }
+        println(maxCalories.sortedDescending().take(3).sum())
+    }
 }
--- advent-of-code-2022/src/main/kotlin/Main.kt
+++ advent-of-code-2022/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
 fun main(args: Array<String>) {
     Day01().part1()
+    println("part 2:")
+    Day01().part2()
 }
