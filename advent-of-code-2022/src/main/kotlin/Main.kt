a28543dc02203abf78886bba5db32b37566082df
JonasJore <jonas.jore@gmail.com>
2022-12-03 14:23:44 +0100

    AoC 2022 - Day3

--- advent-of-code-2022/src/main/kotlin/Main.kt
+++ advent-of-code-2022/src/main/kotlin/Main.kt
@@ -3,7 +3,11 @@ fun main() {
         "Day 1 part 1:",
         Day01().part1(),
         "Day 1 part 2:",
-        Day01().part2()
+        Day01().part2(),
+        "Day 3 part 1:",
+        Day03().part1(),
+        "Day 3 part 2:",
+        Day03().part2()
     )
 }
 
