508d48266cd1425e1ba08ba4ae21650c5a2a76fa
JonasJore <jonas.jore@gmail.com>
2024-02-09 13:52:48 +0100

    aoc 2015 day 1 done :coffee:

--- advent-of-code-2015/src/main.rs
+++ advent-of-code-2015/src/main.rs
@@ -1,3 +1,41 @@
+use std::fs;
+
+fn aoc_01_part_1() {
+    let file = fs::read_to_string("src/input/aoc_01.txt").expect("error");
+    let mut floor: i32 = 0;
+    file.trim().split("").into_iter().for_each(|item| {
+        // if item == "(" {
+        //     floor += 1;
+        // } else if item == ")" {
+        //     floor -= 1;
+        // }
+        match item {
+            "(" => floor += 1,
+            ")" => floor -= 1,
+            _ => {}
+        };
+    });
+
+    println!("{}", floor);
+}
+
+fn aoc_01_part_2() {
+    let file = fs::read_to_string("src/input/aoc_01.txt").expect("error");
+    let mut floor: i32 = 0;
+    for (index, item) in file.trim().split("").into_iter().enumerate() {
+        match item {
+            "(" => floor += 1,
+            ")" => floor -= 1,
+            _ => {}
+        };
+        if floor == -1 {
+            println!("went into -1 at {}", index);
+            break;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    aoc_01_part_1();
+    aoc_01_part_2();
 }
