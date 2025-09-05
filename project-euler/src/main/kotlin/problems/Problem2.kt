e752ada75ec7408c5717b97667a7bfb60c489331
Jonas Jore <jonas.jore@gmail.com>
2025-09-05 16:18:34 +0200

    project-euler problem 2

--- project-euler/src/main/kotlin/problems/Problem2.kt
+++ project-euler/src/main/kotlin/problems/Problem2.kt
@@ -1,6 +1,22 @@
 package problems
 
+fun fibonacciSequence() = sequence {
+    var a = 1
+    var b = 2
+    yield(a)
+    yield(b)
+    while (true) {
+        val next = a + b
+        yield(next)
+        a = b
+        b = next
+    }
+}
+
 fun problem2() {
     print("Problem 2: ")
-    
+    val seq = fibonacciSequence().takeWhile { it <= 4_000_000 }.toList()
+    val res = seq.filter { it % 2 == 0 }
+
+    print(res.sum())
 }
